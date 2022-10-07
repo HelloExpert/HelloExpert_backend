0f97ca7587c9792ddd6c52878ae64f50e0175e4c
SprengersH <86893159+SprengersH@users.noreply.github.com>
2022-10-07 10:34:14 +0200

    -Removed @RestController function from Application -Added StudentController class with the controller functionality including a new pathway on the localhost.

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,31 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController // to test endpoints // todo made for testing, can remove later?
+
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	// restful endpoint to test with
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(
-				1L,
-				"Hans",
-				"hans.sprengers@fontys.nl",
-				LocalDate.of(1988, 10,19), 33)
-		);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
